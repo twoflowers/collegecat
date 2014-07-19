c8dd43b22e3c4df5c08ea3525a15cc5fb8eb51b3
Tom Martin <tmartin@codero.com>
2014-07-19 10:59:23 -0500

    added keys

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 
-
-CODERO_API_KEY = 'YOU API KEY HERE'
-CODERO_API_URL = 'https://apis.codero.com/cloud/v1/'
-CODERO_API_CODELET = "NEC9TVgKWsMqnwvOVonEWUSI9uM="
-CODERO_API_BILLING_TYPE = "hourly"
+simplify.public_key = "YOUR_PUBLIC_API_KEY"
+simplify.private_key = "YOUR_PRIVATE_API_KEY"
 
 SRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
