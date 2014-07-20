c0b9d554dbb92f36657ddab0fbf8e2685bb5036a
Marc Streeter <marc.streeter@mavs.uta.edu>
2014-07-20 07:15:36 -0500

    added flask-login required methods to User

--- app/models.py
+++ app/models.py
@@ -195,6 +195,19 @@ class User(db.Model):
            'payment': self.payment
        }
 
+    def is_authenticated(self):
+        return True if self.id else False
+        #Returns True if the user is authenticated, i.e. they have provided valid credentials. (Only authenticated users will fulfill the criteria of login_required.)
+    def is_active(self):
+        return True
+        #Returns True if this is an active user - in addition to being authenticated, they also have activated their account, not been suspended, or any condition your application has for rejecting an account. Inactive accounts may not log in (without being forced of course).
+    def is_anonymous(self):
+        return not self.is_authenticated()
+        #Returns True if this is an anonymous user. (Actual users should return False instead.)
+    def get_id(self):
+        return self.id
+        #Returns a unicode that uniquely identifies this user, and can be used to load the user from the user_loader callback. Note that this must be a unicode - if the ID is natively an int or some other type, you will need to convert it to unicode.
+
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
