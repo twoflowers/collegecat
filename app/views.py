3f0b5bf0674066c3afdb7599cd31fd065a7ac17c
Marc Streeter <marc.streeter@mavs.uta.edu>
2014-07-19 21:50:42 -0500

    improved restify

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from app import app
 from flask.ext import restful
 from flask.ext.restful import reqparse
 from flask import render_template
+import errors
 
 api = restful.Api(app)
 
@@ -24,12 +25,34 @@ class Ratings(restful.Resource):
 api.add_resource(Ratings, '/api/rating/<int:tutor_id>')
 
 
+
 @app.route('/')
 def index():
     return render_template('base.html')
 
 
-def restify(data, status=200):
-    status = int(status)
+# Errors
+@app.errorhandler(errors.SystemError)
+def handle_system_error(error):
+    return restify(error)
+
+
+@app.errorhandler(errors.InvalidUsage)
+def handle_invalid_usage(error):
+    return restify(error)
+
+
+def restify(data, status=None):
+
+    if isinstance(data, Exception):
+        status = int(status) or data.status_code or 500
+        data = {'exception': data.message}
+
+    elif isinstance(data, (list, dict)):
+        status = int(status) or 200
+
+    else:
+        raise errors.SystemError("Restify")
+
     return {'data': data,
             'status': status}, status
