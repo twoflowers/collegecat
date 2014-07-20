108094ecacf7c62c8806e98b0823f2611fea2eb1
Marc Streeter <marc.streeter@mavs.uta.edu>
2014-07-20 07:14:23 -0500

    added default message

--- app/errors.py
+++ app/errors.py
@@ -1,9 +1,9 @@
 class Error(Exception):
     status_code = 400
-
+    default_message = "General Error"
     def __init__(self, message, status_code=None, payload=None):
         Exception.__init__(self)
-        self.message = message
+        self.message = self.default_message + " : " + message
         if status_code is not None:
             self.status_code = status_code
         self.payload = payload
@@ -16,9 +16,12 @@ class Error(Exception):
 
 class InvalidUsage(Error):
     status_code = 400
+    default_message = "Invalid Usage"
 
 class Permission(Error):
     status_code = 403
+    default_message = "Not Permitted"
 
 class SystemError(Error):
     status_code = 500
+    default_message = "System Error"
